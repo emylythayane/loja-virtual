15e6c5ec521d0e8d1469390877e8d3912138a9bc
emylythayane <emylythayne457@gmail.com>
2026-09-09 20:42:10 -0300

    criar o arquivo app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+function calculartotal (itens) {
+      let total = 0
+     
+   for (let i = 0; i < itens.length; i++){
+        total += itens [i].preco
+   }
+
+   // aplica desconto de fidelicade
+   // antes de retornar o valor final 
+
+   return total 
